286454d504236effe03a564394573f6ea53a798f
claudiocsx <claudiosasuke@gmail.com>
2022-07-23 14:22:45 -0300

    contruini o active

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,23 @@
 
 /*=============== FEATURES ===============*/
 
+console.log('pegou')
+const SelectCard = document.querySelectorAll('.feature__cads')
+      selectIcon = document.querySelectorAll('.feature__icon')
 
+function activeCard(){
+    SelectCard.forEach(l => l.classList.remove('select__feature'))
+    this.classList.add('select__feature')
+}
+
+function activeIcon(){
+    selectIcon.forEach(i => i.classList.remove('select__icon'))
+    this.classList.add('select__icon')
+}
+
+SelectCard.forEach(l => l.addEventListener('click', activeCard))
+
+selectIcon.forEach(i => i.addEventListener('click', activeIcon))
 /*=============== TESTIMONIALS ===============*/
 
 
