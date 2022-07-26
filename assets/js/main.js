409e55ef1300aff5e0e7353c08aaf3d254ec6dab
AlisonLima <alison.juniolima@gmail.com>
2022-07-26 09:25:19 -0300

    Contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,40 @@
 /*=============== HEADER ===============*/
 
-
 /*=============== HOME ===============*/
-
 
 /*=============== FEATURES ===============*/
 
-
 /*=============== TESTIMONIALS ===============*/
-
 
 /*=============== CONTENT ===============*/
 
-
 /*=============== PRICING ===============*/
-
 
 /*=============== CONTACT ===============*/
 
+// Email modal
+function modalInit(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.classList.add('modal--show');
+}
+
+const button = document.getElementById('subscribe');
+
+button.addEventListener('click', function (event) {
+  event.preventDefault;
+  modalInit('modal--success');
+});
+//close modal
+function modalClose(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.classList.remove('modal--show');
+}
+
+const buttonClose = document.getElementById('close');
+
+buttonClose.addEventListener('click', function (event) {
+  event.preventDefault;
+  modalClose('modal--success');
+});
 
 /*=============== FOOTER ===============*/
